84cedae00225fcd2cb413bdc5b5a06b825cf8145
Marc <marc.smith@homtail.de>
2025-05-21 16:05:40 -0400

    Fix: Add utils.js to root and update all imports to use it

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cn } from '@/lib/utils';
+import { cn } from '../../utils';
 
 export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
 

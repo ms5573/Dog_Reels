56fbe7fbfbbff92e4790cc1dd24b2006fac6b883
Marc <marc.smith@homtail.de>
2025-05-21 14:59:30 -0400

    Fix: Use relative paths for @/lib/utils in UI components

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 
-import { cn } from "@/lib/utils" // Assuming you have a lib/utils.ts for cn
+import { cn } from "../../lib/utils"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
